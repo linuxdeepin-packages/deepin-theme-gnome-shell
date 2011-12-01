b823c3859378e9f7058e1a9d5c5f2a853ee31b3a
Andy Stewart <lazycat.manatee@gmail.com>
2011-12-01 18:42:36 +0800

    Update repos.sh

--- repos.sh
+++ repos.sh
@@ -8,7 +8,7 @@ case "$1" in
         git pull origin master
         ;;
     "push" )
-        git push git@github.com:manateelazycat/deepin-software-center.git
+        git push git@github.com:manateelazycat/deepin-gs-theme.git
         ;;
     * ) 
         echo "Help"
